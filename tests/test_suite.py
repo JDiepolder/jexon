3b3cda6c6598fca6b8da17427aed5780ce19e761
JDiepolder <Johannes.Diepolder@gmail.com>
2022-12-31 20:35:02 +0300

    Update test_suite.py

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -14,14 +14,8 @@ def get_stdouterr_from_popen(cmd):
     return (str(stdout.decode("utf-8")), str(stderr.decode("utf-8")))
 
 def run_and_load(test_name):
-    stdout, stderr = get_stdouterr_from_popen('ls')
-    print(stdout)
-    stdout, stderr = get_stdouterr_from_popen('ls ..')
-    print(stdout)
-    stdout, stderr = get_stdouterr_from_popen('ls ../jexon')
-    print(stdout)
     _, stderr = get_stdouterr_from_popen(
-        'python3 -m jexon.execute "tests/test_{}.json" "tests/output_{}.json" "tests/config.json"'.format(test_name, test_name))
+        'python3 -m jexon.execute "tests/test_{}.json" "tests/output_{}.json" "tests/config.json"'.format(test_name))
 
     if stderr:
         print(stderr)
@@ -31,22 +25,20 @@ def run_and_load(test_name):
 
     with open("tests/expected_{}.json".format(test_name)) as f:
         expected = json.load(f)
-        
+    
     return stderr, output, expected
 
 
 class TestGeneric(unittest.TestCase):
 
-    def test_flat(self):
-        test_name = "flat"
+    def run_test(self, test_name):
         stderr, output, expected = run_and_load(test_name)
         self.assertEqual(stderr, '', "Test run {} failed.".format(test_name))
         self.assertEqual(output, expected, "Output is not as expected.")
         os.remove("tests/output_{}.json".format(test_name))
 
+    def test_flat(self):
+        self.run_test("flat")
+
     def test_flat_nested(self):
-        test_name = "flat_nested"
-        stderr, output, expected = run_and_load(test_name)
-        self.assertEqual(stderr, '', "Test run {} failed.".format(test_name))
-        self.assertEqual(output, expected, "Output is not as expected.")
-        os.remove("tests/output_{}.json".format(test_name))
+        self.run_test("flat_nested")
