8bcf7467475fb6c52c07a1393f85e1bd5b7faad8
JDiepolder <Johannes.Diepolder@gmail.com>
2022-12-31 19:47:06 +0300

    Update test_suite.py

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -15,58 +15,40 @@ def get_stdouterr_from_popen(cmd):
     return (str(stdout.decode("utf-8")), str(stderr.decode("utf-8")))
 
 
-class TestCase(unittest.TestCase):
-    def __init__(self, test_name):
-        super(TestCase, self).__init__("test_generic")
-        self.test_name = test_name
+class TestGeneric(unittest.TestGeneric):
+    def __init__(self):
+        super(TestGeneric, self).__init__("test_generic")
 
     def setUp(self):
-        print("\n> test setup: " + self.test_name)
+        self.test_files = glob.glob('test_*.json')
         pass
 
     def test_generic(self):
-        stdout, stderr = get_stdouterr_from_popen(
-            'cd ../jexon && ls && python -m jexon.execute "tests/test_{test_name}.json" "tests/output_{test_name}.json" "tests/config.json"'.format(test_name=self.test_name))
-            # 'ls ../jexon'.format(test_name=self.test_name))
-        
-        print(stdout)
-        
-        if stderr:
-            print(stderr)
-        self.assertEqual(
-            stderr, '', "Test run {} failed.".format(self.test_name))
+        for test_file in self.test_files:
 
-        with open("output_{}.json".format(self.test_name)) as f:
+            test_name = test_file[5:-5]
+
+            _, stderr = get_stdouterr_from_popen(
+                'cd .. && python3 -m jexon.execute "tests/test_{}.json" "tests/output_{}.json" "tests/config.json"'.format(test_name))
+
+            if stderr:
+                print(stderr)
             self.assertEqual(
-                f.closed, False, "Output file for {} could not be opened.".format(self.test_name))
-            output = json.load(f)
+                stderr, '', "Test run {} failed.".format(test_name))
 
-        with open("expected_{}.json".format(self.test_name)) as f:
-            self.assertEqual(
-                f.closed, False, "Expected file for {} could not be opened.".format(self.test_name))
-            expected = json.load(f)
+            with open("output_{}.json".format(test_name)) as f:
+                self.assertEqual(
+                    f.closed, False, "Output file for {} could not be opened.".format(test_name))
+                output = json.load(f)
 
-        self.assertEqual(output, expected, "Output is not as expected.")
+            with open("expected_{}.json".format(test_name)) as f:
+                self.assertEqual(
+                    f.closed, False, "Expected file for {} could not be opened.".format(test_name))
+                expected = json.load(f)
 
-        os.remove("output_{}.json".format(self.test_name))
+            self.assertEqual(output, expected, "Output is not as expected.")
+
+            os.remove("output_{}.json".format(test_name))
 
     def tearDown(self):
-        print("\n< test teardown: " + self.test_name)
         pass
-
-
-def suite():
-    suite = unittest.TestSuite()
-    testFiles = glob.glob('test_*.json')
-    for testFile in testFiles:
-        print(testFile)
-        suite.addTest(TestCase(testFile[5:-5]))
-    return suite
-
-
-if __name__ == '__main__':
-    print("##############################################################################")
-    print("# Running jexon Test Suite")
-    print("##############################################################################")
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
